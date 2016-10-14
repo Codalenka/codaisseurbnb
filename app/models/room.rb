9a771c9d9381a56d7ba81f3ecf2d212783b85fbd
MalenkavanOmmen <malenkavanommen@gmail.com>
2016-10-14 10:49:07 +0200

    Through Association: Models

--- app/models/room.rb
+++ app/models/room.rb
@@ -20,5 +20,10 @@ class Room < ApplicationRecord
   def self.order_by_price
     order(:price)
   end
+
+
+  has_many :bookings, dependent: :destroy
+
+  has_many :guests, through: :bookings, source: :user
 end
 
